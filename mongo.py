b3f1e116f29a201de64e31bab591c2b1cab9a9df
karthickai <karthickps.ai@gmail.com>
2019-11-11 12:28:02 -0800

    validate initial method

--- mongo.py
+++ mongo.py
@@ -2,8 +2,41 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
-print(myclient.list_database_names())
+db = myclient.iotb
 
-with myclient:
-    db = myclient.user
-    print(db.collection_names())
+
+def validate(data):
+    users = db.users.find()
+    for user in users:
+        if user[_id] == data["clientId"]:
+            print("Authorised")
+            break
+    print("Unauthorised")
+
+
+import paho.mqtt.client as mqtt
+import json
+
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
+
+
+def on_message(mqttc, obj, msg):
+    m_decode=str(msg.payload.decode("utf-8","ignore"))
+    data = json.loads(m_decode)
+    validate(data)
+
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+
+
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.connect("127.0.0.1", 1883, 60)
+mqttc.subscribe("/iotb/request", 0)
+
+mqttc.loop_forever()
